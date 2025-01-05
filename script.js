ff8b6799e03a18baff5d60757d468dce5c4e249d
Antony Albuquerque <118316078+AlbuquerquexD@users.noreply.github.com>
2025-01-05 01:40:03 -0300

    commit inicial da aplicação

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const countdown = () => {
+    const newYear = new Date('December 31, 2025 23:59:59');
+    const currentDate = new Date();
+    const totalSeconds = (newYear - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+
+};
+
+countdown();
+setInterval(countdown,1000)
